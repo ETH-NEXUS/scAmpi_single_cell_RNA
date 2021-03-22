e3cb6ceb2f840e84e208ec07e431383960573361
Franziska Singer <singer@nexus.ethz.ch>
2021-03-22 15:34:06 +0100

    Further cleanup

--- doc/createRuleGraph.sh
+++
@@ -1,5 +0,0 @@
-snakemake -s snake_example.snake --configfile /config_clinical.json --rulegraph > rulegraph_TP_master.dot
-
-module load gcc/4.8.2 graphviz/2.38
-
-dot -Tpng rulegraph_TP_master.dot > rulegraph_TP_master.png
--- doc/sc_transcriptomics_rules_dag.sh
+++
@@ -1,1 +0,0 @@
-snakemake --dag -s tumor_profiler.snakefile | dot -Tsvg > dag_sc_transcriptomics_rules.svg
