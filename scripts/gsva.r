c1c417bb29d253b277e1d0e42f7c215a43824e3a
Anne Bertolini <bertolini@nexus.ethz.ch>
2021-12-13 17:31:13 +0100

    [DOC] Tidy up gsva.r

--- scripts/gsva.r
+++ scripts/gsva.r
@@ -1,21 +1,20 @@
-
 ################################################################################
-## cell type classification - GSVA
-## NOTE: this is an independent piece of analysis. 
-##       all output files, including plots, will already be handled in this script
+## GSVA analysis
+## Author: Michael Prummer
+## Adapted: Anne Bertolini
 ################################################################################
 
-lby = c("optparse", "scran", "reshape2", "uwot", "GSVA", "aroma.light",
-        "ggplot2", "pheatmap","RColorBrewer", "limma", "viridis", "dplyr")
-resp = lapply(lby, require, character.only=T, warn.conflicts=F, quietly=T)
-if(!all(unlist(resp))) stop("Could not load one or more packages")
+lby <- c("optparse", "scran", "reshape2", "uwot", "GSVA", "aroma.light",
+        "ggplot2", "pheatmap", "RColorBrewer", "limma", "viridis", "dplyr")
+resp <- lapply(lby, require, character.only = T, warn.conflicts = F, quietly = T)
+if (!all(unlist(resp))) stop("Could not load one or more packages")
 rm(resp, lby)
 
-# options(error=function()traceback(2))
+# general options
 options(stringsAsFactors = FALSE)
-fontsize = theme(axis.text=element_text(size=9), axis.title=element_text(size=11))
+fontsize <- theme(axis.text = element_text(size = 9), axis.title = element_text(size = 11))
 theme_set(theme_bw(12) + fontsize)
-col.pal = colorRampPalette( rev(brewer.pal(11, "RdBu")) )(255)
+col.pal <- colorRampPalette(rev(brewer.pal(11, "RdBu")))(255)
 
 # give out session Info
 cat("\n\n\nPrint sessionInfo:\n\n")
@@ -23,194 +22,162 @@ print(sessionInfo())
 cat("\n\n\n\n")
 
 # convenience function for string concatenation
-'%&%' = function(a,b) paste(a,b,sep="")
+"%&%" <- function(a, b) paste(a, b, sep = "")
 
 # command line arguments are parsed
-option_list = list(
+option_list <- list(
   make_option("--SCE", type = "character", help = "Path to sce object file with input data (sce_celltypes_noatypical.RDS)."),
   make_option("--outputDirec", type = "character", help = "Path to the directory where output files will be written."),
   make_option("--geneset", type = "character", help = "Hallmark geneset library gmt file."),
   make_option("--sampleName", type = "character", help = "Sample identifier. Attached to each output name.")
 )
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
 
-opt_parser = OptionParser(option_list = option_list)
-opt = parse_args(opt_parser)
-
-path = opt$outputDirec %&% opt$sampleName
-
-print(opt$SCE)
-
-path = opt$outputDirec %&% opt$sampleName
+path <- opt$outputDirec %&% opt$sampleName
 
 
 ################################################################################
 ## main code starts here
 ################################################################################
+
 ## load input data
-sce_data = readRDS(opt$SCE)
+print(opt$SCE)
+sce_data <- readRDS(opt$SCE)
 
 # define color scheme
-all.cell.types = metadata(sce_data)$all_celltypes
+all.cell.types <- metadata(sce_data)$all_celltypes
 cols33 <- c("red2", "green4", "blue2", "cyan2", "yellow1", "purple", "brown",
             "chocolate1", "chartreuse2", "darkgoldenrod3", "steelblue1", "slateblue3", "olivedrab4", "gold2",
-            "violetred3", "darkcyan","orchid3", "darksalmon", "darkslategrey", "khaki", "indianred2","magenta", "slategray2", 
+            "violetred3", "darkcyan", "orchid3", "darksalmon", "darkslategrey", "khaki", "indianred2", "magenta", "slategray2",
             "olivedrab1", "mediumaquamarine", "hotpink", "yellow3",
             "bisque4", "darkseagreen1", "dodgerblue3",
             "deeppink4", "sienna4", "mediumorchid4")
-ct.color = c(cols33[seq(length(all.cell.types))], "grey50", "black")
+number_celltypes <- length(all.cell.types)
+ct.color <- c(cols33[seq(number_celltypes)], "grey50", "black")
 
 ################################################################################
-#
-# signature expression painting on tSNE plots (phenograph)
-#
+
 # load ref.gene.list
 # make sure all columns are captured when reading in the gene set lists in gmt format
-tmp = readLines(opt$geneset)
-tmp = lapply(tmp, function(x) strsplit(x, "\\\t")[[1]])
-names(tmp) = sapply(tmp, function(x) x[1])
+tmp <- readLines(opt$geneset)
+tmp <- lapply(tmp, function(x) strsplit(x, "\\\t")[[1]])
+names(tmp) <- sapply(tmp, function(x) x[1])
 # remove gene set names from gene list
-gset = sapply(tmp, function(x) x[-1])
+gset <- sapply(tmp, function(x) x[-1])
 # remove gene set description from gene list
-gset = sapply(gset, function(x) x[-1])
-names(gset) = gsub("HALLMARK_", "", names(gset))
+gset <- sapply(gset, function(x) x[-1])
+names(gset) <- gsub("HALLMARK_", "", names(gset))
 
 # select subset gene expression matrix
-idxs = ids2indices(gset, rowData(sce_data)$SYMBOL)
-all.genes = unique(as.numeric(unlist(idxs)))
-             
-t.m = assay(sce_data, "pearson_resid")[all.genes,]
-             
+idxs <- ids2indices(gset, rowData(sce_data)$SYMBOL)
+all.genes <- unique(as.numeric(unlist(idxs)))
+t.m <- assay(sce_data, "pearson_resid")[all.genes, ]
+
 # estimate geneset-sample matrix from gene-sample matrix
-rgsa = gsva(t.m, gset, method="gsva")
-# plot
-gsva_results = melt(rgsa)
-names(gsva_results) = c("gene.set", "barcodes", "value")
+rgsa <- gsva(t.m, gset, method = "gsva")
+# prepare gsva results for plotting
+gsva_results <- melt(rgsa)
+names(gsva_results) <- c("gene.set", "barcodes", "value")
 
 # get UMAP coordinates
-umap_coord = reducedDim(sce_data, "umap_hvg")
-umap_coord = as.data.frame(umap_coord)
-umap_coord$barcodes = rownames(umap_coord)
+umap_coord <- reducedDim(sce_data, "umap_hvg")
+umap_coord <- as.data.frame(umap_coord)
+umap_coord$barcodes <- rownames(umap_coord)
 
 # add UMAP coordinates to cell meta data
-tomerge = dplyr::left_join(umap_coord, as.data.frame(colData(sce_data)), by = "barcodes")
+tomerge <- dplyr::left_join(umap_coord, as.data.frame(colData(sce_data)), by = "barcodes")
 stopifnot(tomerge$barcodes == as.data.frame(colData(sce_data))$barcodes)
 
 # merge gsva results with cell meta data
 # preserve order of cells in `gsva_results`
-t.plot = dplyr::left_join(gsva_results, tomerge, by = "barcodes")
+t.plot <- dplyr::left_join(gsva_results, tomerge, by = "barcodes")
 stopifnot(t.plot$barcodes == gsva_results$barcodes)
-print("str(t.plot):")
-print(str(t.plot))
-print("summary(t.plot):")
-print(summary(t.plot))
 
 # Trim outlier values
 t.plot$value_limited <- t.plot$value
 perc_1 <- quantile(t.plot$value, prob = 0.01)
 perc_99 <- quantile(t.plot$value, prob = 0.99)
 t.plot$value_limited[t.plot$value_limited <= perc_1] <- perc_1
-print("Number of data points set to perc_1:")
+cat("\n\nNumber of data points set to perc_1:\n")
 print(table(t.plot$value_limited == perc_1))
+
 t.plot$value_limited[t.plot$value_limited >= perc_99] <- perc_99
-print("Number of data points set to perc_99:")
+cat("\n\nNumber of data points set to perc_99:\n")
 print(table(t.plot$value_limited == perc_99))
-print("str(t.plot$value_limited):")
-print(str(t.plot$value_limited))
-print("summary(t.plot$value_limited) after trimmming the outliers:")
-print(summary(t.plot$value_limited))
-t.plot$value_limited <- round(t.plot$value_limited, digits = 2)
-break_low <- min(t.plot$value_limited)
-print(break_low)
-break_high <- max(t.plot$value_limited)
-print(break_high)
-label_low <- paste("<=", break_low)
-print(label_low)
-label_high <- paste(">=", break_high)
-print(label_high)
-#t.plot$value_log <- log10(t.plot$value + 1)
-print("summary(t.plot$value_limited) after rounding the values to two digits:")
+
+cat("\nsummary(t.plot$value_limited) after trimmming the outliers:")
 print(summary(t.plot$value_limited))
 
-pp = ggplot(t.plot, aes(x=V1, y=V2) ) +
-  geom_point(aes(color=value_limited), size=1) + xlab("umap-1") + ylab("umap-2") +
+t.plot$value_limited <- round(t.plot$value_limited, digits = 2)
+break_low <- min(t.plot$value_limited)
+cat("\nbreak_low:", break_low)
+break_high <- max(t.plot$value_limited)
+cat("\nbreak_high:", break_high)
+label_low <- paste("<=", break_low)
+cat("\nlabel_low:", label_low)
+label_high <- paste(">=", break_high)
+cat("\nlabel_high:", label_high)
+
+# plot UMAP with gsva results
+pp <- ggplot(t.plot, aes(x = V1, y = V2)) +
+  geom_point(aes(color = value_limited), size = 1) +
+  xlab("umap-1") +
+  ylab("umap-2") +
   theme(panel.background = element_rect(fill = "grey80")) +
-  scale_color_distiller(name="", palette = "RdBu",
+  scale_color_distiller(name = "", palette = "RdBu",
                         breaks = c(break_low, 0, break_high),
                         labels = c(label_low, "0.00", label_high)) +
-  facet_wrap(~gene.set, ncol=6)
+  facet_wrap(~gene.set, ncol = 6)
 ggsave(path %&% ".gsetscore_hvg_umap.png", pp,
        dpi = 600, width = 50, height = 50, units = "cm")
 
-# pp = ggplot(t.plot, aes(x=V1, y=V2, color=celltype_final) ) + 
-#   geom_point(size=1) + xlab("umap-1") + ylab("umap-2") + 
-#   guides(colour = guide_legend(override.aes = list(size=2, shape=15), ncol=1))+
-#   scale_color_manual(name="", values = ct.color, drop=F)
-# ggsave(path %&% ".ctfinal_hvg_umap.png", pp,
-#        dpi = 600, width = 20, height = 15, units = "cm")
-# 
-# pp = ggplot(t.plot, aes(x=V1, y=V2, color=factor(umap_cl)) ) + 
-#   geom_point(size=1) + xlab("umap-1") + ylab("umap-2") + 
-#   guides(colour = guide_legend(override.aes = list(size=2, shape=15), ncol=1))+
-#   scale_color_manual(name="", values = ct.color, drop=F)
-# ggsave(path %&% ".hvgcl_hvg_umap.png", pp,
-#        dpi = 600, width = 20, height = 15, units = "cm")
-# 
-# pp = ggplot(t.plot, aes(x=V1, y=V2, color=factor(phenograph_clusters)) ) + 
-#   geom_point(size=1) + xlab("umap-1") + ylab("umap-2") + 
-#   guides(colour = guide_legend(override.aes = list(size=2, shape=15), ncol=1))+
-#   scale_color_manual(name="", values = ct.color, drop=F)
-# ggsave(path %&% ".phcl_hvg_umap.png", pp,
-#        dpi = 600, width = 20, height = 15, units = "cm")
 
-# save sce object for gsva-scores instead of gene counts for later use 
-sce_gsva = SingleCellExperiment(assays=list(gsva = rgsa), 
+# save sce object for gsva-scores instead of gene counts for later use
+sce_gsva <- SingleCellExperiment(assays = list(gsva = rgsa),
                                 colData = colData(sce_data),
                                 metadata = gset)
 saveRDS(sce_gsva, path %&% ".sce_gsva.RDS")
-# optional: save geneset-cell score matrix.
-# write.table(rgsa, path %&% ".geneset_scores.tsv", sep="\t", col.names = NA)
 
 
 ################################################################################
 ## heatmaps of geneset-cell matrix, with phcl-annotation.
-annot.col = data.frame(Phenograph=factor(tomerge$phenograph_clusters),
-                       Cell.type=tomerge$celltype_final)
-rownames(annot.col) = tomerge$barcodes
-# levels(annot.col$Phenograph) = levels(annot.col$Phenograph) %&% " (" %&% 
-#   clu.pu[-nrow(clu.pu)+c(0,1), ncol(clu.pu)-1] %&% ")"
-# rownames(annot.col) = colnames(rgsa)
+
+# prepare annotation column for heatmap
+annot.col <- data.frame(Phenograph = factor(tomerge$phenograph_clusters),
+                       Cell.type = tomerge$celltype_final)
+rownames(annot.col) <- tomerge$barcodes
 stopifnot(rownames(annot.col) == colnames(rgsa))
 
 # have clusters sorted to colours
 all_clusters <- levels(annot.col$Phenograph)
 number_clusters <- length(all_clusters)
-ancol.col = cols33[seq(number_clusters)]
-names(ancol.col) = all_clusters
+cluster_colors <- cols33[seq(number_clusters)]
+names(cluster_colors) <- all_clusters
 
 # have cell types sorted to colours
-ctan.col = ct.color
-names(ctan.col) = levels(annot.col$Cell.type)
+celltype_colors <- ct.color
+names(celltype_colors) <- levels(annot.col$Cell.type)
 
 # have colours for clusters and cell types together
-ancolor = list(Phenograph=ancol.col, Cell.type=ctan.col)
+plotting_colors <- list(Phenograph = cluster_colors, Cell.type = celltype_colors)
 
-col.pal = colorRampPalette( rev(brewer.pal(11, "RdBu")) )(255)
+col.pal <- colorRampPalette(rev(brewer.pal(11, "RdBu")))(255)
 
 # have gsva results ordered by clusters
-rgsa_plotting = rgsa[ , order(annot.col$Phenograph)]
+rgsa_plotting <- rgsa[, order(annot.col$Phenograph)]
 
 # plot heatmap
-hm1 = pheatmap(rgsa_plotting,
-               scale="none",
+hm1 <- pheatmap(rgsa_plotting,
+               scale = "none",
                clustering_method = "ward.D2",
                show_colnames = F,
                color = col.pal,
                cluster_cols = F,
                annotation_col = annot.col,
-               annotation_colors = ancolor,
+               annotation_colors = plotting_colors,
                annotation_names_col = T,
                fontsize_row = 8)
 
 ggsave(path %&% ".gsetscore_hm.png", hm1$gtable,
        width = 30, height = 24, units = "cm", dpi = 600)
-
