dd321a7127df3aae6f1b23bcb7b0c644bfcff44e
Anne Bertolini <bertolini@nexus.ethz.ch>
2022-03-18 14:48:32 +0100

    [FEAT] Adapt testdata to config file in yaml format
    In testdata, use the yaml config file.
    Adapt the script `prepare_testrun.sh` to work with yaml instead of json.

--- testdata/prepare_testrun.sh
+++ testdata/prepare_testrun.sh
@@ -28,44 +28,50 @@ mkdir -p snake_files
 
 # copy example config file and sample map to the testrun directory and replace placeholders with correct path
 
-cp ${scAmpi_path}/testdata/config_scAmpi_testdata.json snake_files/
+cp ${scAmpi_path}/testdata/config_scAmpi_testdata.yaml snake_files/
 cp ${scAmpi_path}/testdata/sample_map_testdata.tsv snake_files/
 
 # replace test directory path
-while read line; do
-	    echo ${line//testdir/${testrun_dir}}
-    done < snake_files/config_scAmpi_testdata.json > snake_files/config_scAmpi_testdata_temp.json
-    mv snake_files/config_scAmpi_testdata_temp.json snake_files/config_scAmpi_testdata.json
+# temporarily adapt IFS to preserve leading whitespaces
+OLD_IFS="$IFS"
+IFS=
+while read -r line; do
+    echo ${line//testdir/${testrun_dir}}
+    done < snake_files/config_scAmpi_testdata.yaml > snake_files/config_scAmpi_testdata_temp.yaml
+    mv snake_files/config_scAmpi_testdata_temp.yaml snake_files/config_scAmpi_testdata.yaml
 
 # replace reference directory path
-while read line; do
-	    echo ${line//reference_dir/${cellranger_ref}}
-    done < snake_files/config_scAmpi_testdata.json > snake_files/config_scAmpi_testdata_temp.json
-    mv snake_files/config_scAmpi_testdata_temp.json snake_files/config_scAmpi_testdata.json
+while read -r line; do
+    echo ${line//reference_dir/${cellranger_ref}}
+    done < snake_files/config_scAmpi_testdata.yaml > snake_files/config_scAmpi_testdata_temp.yaml
+    mv snake_files/config_scAmpi_testdata_temp.yaml snake_files/config_scAmpi_testdata.yaml
 
 # replace scAmpi git path
-while read line; do
-	    echo ${line//path_to_scAmpi_git/${scAmpi_path}}
-    done < snake_files/config_scAmpi_testdata.json > snake_files/config_scAmpi_testdata_temp.json
-    mv snake_files/config_scAmpi_testdata_temp.json snake_files/config_scAmpi_testdata.json
+while read -r line; do
+    echo ${line//path_to_scAmpi_git/${scAmpi_path}}
+    done < snake_files/config_scAmpi_testdata.yaml > snake_files/config_scAmpi_testdata_temp.yaml
+    mv snake_files/config_scAmpi_testdata_temp.yaml snake_files/config_scAmpi_testdata.yaml
+
+# restore IFS to default
+IFS="$OLD_IFS"
+
+
+# go to snake_files and prepare dry run and full run scripts
+cd ${testrun_dir}/snake_files/
+echo "snakemake -s ${scAmpi_path}/snake/snake_scAmpi_basic_master.snake --configfile config_scAmpi_testdata.yaml -n" > dryrun_scAmpi.sh
+chmod +x dryrun_scAmpi.sh
+
+echo "bsub -J scAmpi_test -eo ${testrun_dir}/snake_files/scAmpi_test.err -oo ${testrun_dir}/snake_files/scAmpi_test.out -W 23:59 \"snakemake --latency-wait 60 -s ${scAmpi_path}/snake/snake_scAmpi_basic_master.snake --configfile config_scAmpi_testdata.yaml --cluster 'bsub -M {params.mem} -n {threads} -W {params.time} -R \"rusage[mem={params.mem},scratch={params.scratch}]\" -eo {params.lsferrfile} -oo {params.lsfoutfile}' -j 10 -p -k\"" > run_scAmpi.sh
+chmod +x run_scAmpi.sh
+
+echo "snakemake -s ${scAmpi_path}/snake/snake_scAmpi_basic_master.snake --configfile config_scAmpi_testdata.yaml -j 10 -p -k" > run_scAmpi_local.sh
+chmod +x run_scAmpi_local.sh
+
 
 # download and extract example data from the 10xGenomics webpage
-
-cd fastqs
+cd ${testrun_dir}/fastqs/
 wget https://cg.10xgenomics.com/samples/cell-exp/3.0.2/5k_pbmc_v3/5k_pbmc_v3_fastqs.tar
 tar -xvf 5k_pbmc_v3_fastqs.tar
 rm 5k_pbmc_v3_fastqs.tar
 mv 5k_pbmc_v3_fastqs/* .
 rm -r 5k_pbmc_v3_fastqs/
-
-# go to snake_files and prepare dry run and full run scripts
-
-cd ${testrun_dir}/snake_files/
-echo "snakemake -s ${scAmpi_path}/snake/snake_scAmpi_basic_master.snake --configfile config_scAmpi_testdata.json -n" > dryrun_scAmpi.sh
-chmod +x dryrun_scAmpi.sh
-
-echo "bsub -J scAmpi_test -eo ${testrun_dir}/snake_files/scAmpi_test.err -oo ${testrun_dir}/snake_files/scAmpi_test.out -W 23:59 \"snakemake --latency-wait 60 -s ${scAmpi_path}/snake/snake_scAmpi_basic_master.snake --configfile config_scAmpi_testdata.json --cluster 'bsub -M {params.mem} -n {threads} -W {params.time} -R \"rusage[mem={params.mem},scratch={params.scratch}]\" -eo {params.lsferrfile} -oo {params.lsfoutfile}' -j 10 -p -k\"" > run_scAmpi.sh
-chmod +x run_scAmpi.sh
-
-echo "snakemake -s ${scAmpi_path}/snake/snake_scAmpi_basic_master.snake --configfile config_scAmpi_testdata.json -j 10 -p -k" > run_scAmpi_local.sh
-chmod +x run_scAmpi_local.sh
