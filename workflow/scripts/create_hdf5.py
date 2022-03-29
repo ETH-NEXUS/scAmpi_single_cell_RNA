00bac92fe1726a033082c1d07b502392704c1e96
Anne Bertolini <bertolini@nexus.ethz.ch>
2022-03-29 13:44:17 +0200

    [FEAT] Add utils function to create_hdf5.py script
    Add function `cellranger_to_hdf5` from separate utils.py file to main script
    `create_hdf5.py`.

--- workflow/scripts/create_hdf5.py
+++ workflow/scripts/create_hdf5.py
@@ -1,5 +1,38 @@
 import argparse
-from utils import cellranger_to_hdf5
+import numpy as np
+from scipy.io import mmread
+import h5py
+import os
+
+def cellranger_to_hdf5 (genes, matrix_file, barcodes, out_path):
+
+    # this is needed for the filesystems that do not allow file locking
+    os.environ['HDF5_USE_FILE_LOCKING'] = "FALSE"
+    # to read a matrix market file
+    matrix = mmread(matrix_file.__str__()).astype("float32").todense().T
+    matrix = np.asarray(matrix)
+
+    gene_ids = np.genfromtxt(genes.__str__(), dtype='S16')[:,0]
+    gene_names = np.genfromtxt(genes.__str__(), dtype='S16')[:, 1]
+    cell_names = np.genfromtxt(barcodes.__str__(), dtype='S16')
+
+    # removing all-zero-genes accross all cells
+    detected_genes_index = ~(matrix == 0).all(axis=0)
+    matrix = matrix[:,detected_genes_index]
+    gene_ids = gene_ids[detected_genes_index]
+    gene_names = gene_names[detected_genes_index]
+
+
+    f = h5py.File(out_path.__str__(), "w")
+    f.create_dataset(name = 'raw_counts', data = matrix)
+    gg = f.create_group('gene_attrs')
+    gg.create_dataset(name = 'gene_names', data = gene_names)
+    gg.create_dataset(name = 'gene_ids', data = gene_ids)
+    cg = f.create_group('cell_attrs')
+    cg.create_dataset(name = 'cell_names', data = cell_names)
+    cg.create_dataset(name = 'cells_on_rows', data = True)
+
+    f.close()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-g", "--genes_file", required=True, help="tsv file containing gene names")
