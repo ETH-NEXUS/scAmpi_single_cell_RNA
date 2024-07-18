6f053ea7b44776608b9ebc8059df0c9ac43adb2c
Anne Bertolini <bertolini@nexus.ethz.ch>
2024-07-18 16:20:42 +0200

    [FIX] script generate_QC_plots.R
    Reduce memory usage when plotting QC plots.

--- workflow/scripts/generate_QC_plots.R
+++ workflow/scripts/generate_QC_plots.R
@@ -50,9 +50,6 @@ colnames(umi_counts) <- h5read(opt$hdf5File, "cell_attrs/cell_names")
 dim(umi_counts)
 print(head(umi_counts[, 1:3]))
 
-# get vector of all genes
-endog <- h5read(opt$hdf5File, "gene_attrs/gene_ids")
-
 # create data frame with mapping of gene_ids to hgnc gene_names
 ENS2HGNC <- as.data.frame(h5read(opt$hdf5File, "gene_attrs/gene_ids"))
 names(ENS2HGNC) <- "ensembl_gene_id"
@@ -64,11 +61,11 @@ print(head(ENS2HGNC))
 print(dim(ENS2HGNC))
 
 
-# Plot of gene detection probability (fraction of cells where a gene is detected) as a function of gene expression (total gene count)
-use.data <- umi_counts[endog, ]
+# Plot of gene detection probability ####
+# (fraction of cells where a gene is detected) as a function of gene expression (total gene count)
 plotname <- file_name %&% ".transcript_capture_efficiency.png"
 png(plotname, width = 2200, height = 1800, res = 300)
-plot1 <- smoothScatter(log2(rowSums(use.data) + 1), rowSums(use.data > 0) / ncol(use.data),
+plot1 <- smoothScatter(log2(rowSums(umi_counts) + 1), rowSums(umi_counts > 0) / ncol(umi_counts),
   nrpoints = 0,
   xlab = expression(Log[2] ~ "Total gene count"), ylab = "Detection probability",
   main = "Transcript Capture efficiency
@@ -78,18 +75,14 @@ plot1 <- smoothScatter(log2(rowSums(use.data) + 1), rowSums(use.data > 0) / ncol
 dev.off()
 
 
-
-# Histogram of the library sizes in the dataset
-use.data <- umi_counts[endog, ]
+# Histogram of library sizes in the dataset ####
 plotname <- file_name %&% ".histogram_library_sizes.png"
 png(plotname, width = 2000, height = 1800, res = 300)
-hist(colSums(use.data) / 1e6, xlab = "Library size (millions)", breaks = 20, col = "grey80", ylab = "Number of cells", main = "Library Sizes")
+hist(colSums(umi_counts) / 1e6, xlab = "Library size (millions)", breaks = 20, col = "grey80", ylab = "Number of cells", main = "Library Sizes")
 dev.off()
 
 
-
-
-# Histograms with number of detected genes and number of dropout values
+# Histograms with number of detected genes and number of dropout values ####
 plotname <- file_name %&% ".histogram_nodgs_dropout.png"
 png(plotname, width = 3000, height = 1800, res = 300)
 par(mfrow = c(1, 2))
@@ -100,27 +93,24 @@ hist(colSums(umi_counts == 0), xlab = "Number of dropout values", breaks = 20, c
 dev.off()
 
 
-
-
-## Plot genes accounting for the majority of reads ##
-
+# Plot genes accounting for the majority of reads #####
 
 # Sort genes in decreasing order in terms of their expression
-ReadsPerGene <- sort(rowSums(umi_counts[endog, ]), decreasing = TRUE)
+ReadsPerGene <- sort(rowSums(umi_counts), decreasing = TRUE)
 # Cumulative fraction of reads for the sorted genes:
 CumulFraction <- cumsum(ReadsPerGene) / sum(ReadsPerGene)
 
 # Fraction of reads coming from the top N genes calculated per cell:
 for (N in c(25, 50, 100)) {
   topN <- names(ReadsPerGene)[1:N]
-  ReadFraction <- apply(umi_counts[endog, ], 2, function(x) x[topN] / sum(x))
+  ReadFraction <- apply(umi_counts, 2, function(x) x[topN] / sum(x))
   # Percentage of reads coming from the top N genes:
   # (signif just rounds)
   f <- signif(CumulFraction[N] * 100, digits = 3)
   # Produce a boxplot for the fraction of reads coming from the top N genes:
   plotname <- file_name %&% ".top" %&% N %&% "_genes_majority_of_reads.png"
   png(plotname, width = 2200, height = (1800 * (N / 35)), res = 300)
-  title <- paste("Top ", N, " genes account for ", f, "% of the endogenous  reads", sep = "")
+  title <- paste("Top ", N, " genes account for ", f, "% of reads", sep = "")
   boxplot(ReadFraction[N:1, ],
     use.cols = FALSE, horizontal = TRUE, outline = FALSE,
     boxwex = 0.5, names = rev(ENS2HGNC[topN, 2]), col = "orange", main = title,
@@ -129,9 +119,9 @@ for (N in c(25, 50, 100)) {
   dev.off()
 }
 
-###  Plot proportion of mitochondrial (MT) reads
+# Plot proportion of mitochondrial (MT) reads #####
 # Grep all mitochondrial genes:
-mt <- as.vector(ENS2HGNC[endog, 1][grep("^MT-", ENS2HGNC[endog, 2])])
+mt <- as.vector(ENS2HGNC[, 1][grep("^MT-", ENS2HGNC[, 2])])
 print(length(mt))
 print(mt)
 
@@ -144,7 +134,7 @@ quant95 <- quantile(df$y, probs = 0.95)
 quant98 <- quantile(df$y, probs = 0.98)
 
 
-# Plot the fraction of MT reads as a function of number of detected genes:
+# Plot fraction of MT reads as function of number of detected genes ####
 plotname <- file_name %&% ".fraction_MT_reads.png"
 png(plotname, width = 2200, height = 1800, res = 300)
 ggplot(data = df, aes(x = x, y = y)) +
@@ -162,7 +152,8 @@ ggplot(data = df, aes(x = x, y = y)) +
 as function of number of detected genes")
 dev.off()
 
-# Plot histogram of fraction of MT reads
+
+# Plot histogram of fraction of MT reads ####
 plotname <- file_name %&% ".histogram_fraction_MT_reads.png"
 png(plotname, width = 2200, height = 1800, res = 300)
 plot_mt_fraction <- ggplot(data = df, aes(df$y)) +
@@ -174,14 +165,14 @@ plot_mt_fraction
 dev.off()
 
 
-# Fraction of reads mapping to genes that encode for ribosomal proteins
+# Fraction of reads mapping to genes encoding for ribosomal proteins ####
 
 # Calculate fraction of ribosomal reads
-riboall <- as.vector(ENS2HGNC[endog, 1][grep("^(RPL|MRPL|RPS|MRPS)", ENS2HGNC[endog, 2])])
+riboall <- as.vector(ENS2HGNC[, 1][grep("^(RPL|MRPL|RPS|MRPS)", ENS2HGNC[, 2])])
 print(length(riboall))
 print(head(riboall))
 
-ribo_names <- as.vector(ENS2HGNC[endog, 2][grep("^(RPL|MRPL|RPS|MRPS)", ENS2HGNC[endog, 2])])
+ribo_names <- as.vector(ENS2HGNC[, 2][grep("^(RPL|MRPL|RPS|MRPS)", ENS2HGNC[, 2])])
 print(length(ribo_names))
 print(head(ribo_names))
 
@@ -189,9 +180,8 @@ fraction_riboall <- as.data.frame(colSums(umi_counts[riboall, ]) / colSums(umi_c
 names(fraction_riboall) <- "fraction_ribosomal_genes_reads"
 fraction_riboall$number_detected_genes <- colSums(umi_counts > 0)
 head(fraction_riboall)
-class(fraction_riboall$fraction_ribosomal_genes_reads)
 
-# Plot histogram of fraction of reads mapping to ribosomal protein transcripts
+# Plot histogram of fraction of reads mapping to ribosomal protein transcripts ####
 plotname <- file_name %&% ".histogram_fraction_ribosomal_reads.png"
 png(plotname, width = 2200, height = 1800, res = 300)
 ggplot(data = fraction_riboall, aes(fraction_riboall$fraction_ribosomal_genes_reads)) +
@@ -200,6 +190,7 @@ ggplot(data = fraction_riboall, aes(fraction_riboall$fraction_ribosomal_genes_re
   xlab("Fraction of (RPL|MRPL|RPS|MRPS) reads") +
   ylab("Number of cells")
 dev.off()
+
 
 # Plot the fraction of ribosomal reads as a function of number of detected genes:
 plotname <- file_name %&% ".fraction_ribosomal_reads.png"
@@ -215,60 +206,21 @@ encoding for ribosomal proteins")
 dev.off()
 
 
-###   Gene dispersion as a function of their mean expression (Mean-variance trend)
-
-# Normalize for library size (only endogenous genes) and convert to CPMs:
-# norm_umi_counts=sweep(umi_counts,2,colSums(umi_counts[endog,]),FUN="/")*1e6
-
-### using normalised data
-# use.data=norm_umi_counts
-
 # Genes that are detected in at least one cell:
-# DetectedGenes=which(rowSums(use.data)>0)
+DetectedGenes <- which(rowSums(umi_counts) > 0)
 # calculate the genes' mean expression (with pseudocounts):
-# mean_GE=rowMeans(use.data[DetectedGenes, ] +1/ncol(use.data))
+mean_GE <- rowMeans(umi_counts[DetectedGenes, ] + 1 / ncol(umi_counts))
 # calculate the genes' coefficient of variation for:
-# gene_cv=apply(use.data[DetectedGenes, ], 1, function(x) sd(x)/mean(x+1/length(x)) )
-# Log transform expression and cv:
-# X1=log2(mean_GE)
-# Y1=log2(gene_cv+1/ncol(use.data)  )
-# linear fit of log(cv) as a function of log(gene expression):
-# m=lm(Y1[endog] ~ X1[endog])
-# plotname = outdir %&% file_name %&% ".mean-variance_trend_normalised_counts.png"
-# png(plotname, width = 2200, height = 1800, res = 300)
-# scatterplot of log(cv) as a function of log(mean expression)
-# plot(X1[endog],Y1[endog],xlab="log2(mean gene expression)",ylab="log2(coefficent of variation)" ,
-#     main="Gene dispersion as a function of their mean expression (Mean-variance trend)
-#     Counts are normalized for library size (only endogenous genes)
-#     and converted to CPMs",pch='.',cex=2,col="#00000055", cex.main= 1 )
-# Add regression line
-# abline(coef(m)[1],coef(m)[2],col="red",lwd=2,lty=2)
-# Slope in m-v trend according to poisson distribution:
-# abline(0,-0.5,col="grey",lwd=2,lty=2)
-# legend("topright",legend = c("Regression line", "Poisson distribution"), lty=c(2,2), lwd=c(2,2),col=c("red","grey"))
-# dev.off()
-
-
-
-
-### Without normalisation
-use.data <- umi_counts
-
-# Genes that are detected in at least one cell:
-DetectedGenes <- which(rowSums(use.data) > 0)
-# calculate the genes' mean expression (with pseudocounts):
-mean_GE <- rowMeans(use.data[DetectedGenes, ] + 1 / ncol(use.data))
-# calculate the genes' coefficient of variation for:
-gene_cv <- apply(use.data[DetectedGenes, ], 1, function(x) sd(x) / mean(x + 1 / length(x)))
+gene_cv <- apply(umi_counts[DetectedGenes, ], 1, function(x) sd(x) / mean(x + 1 / length(x)))
 # Log transform expression and cv:
 X1 <- log2(mean_GE)
-Y1 <- log2(gene_cv + 1 / ncol(use.data))
+Y1 <- log2(gene_cv + 1 / ncol(umi_counts))
 # linear fit of log(cv) as a function of log(gene expression):
-m <- lm(Y1[endog] ~ X1[endog])
+m <- lm(Y1 ~ X1)
 plotname <- file_name %&% ".mean-variance_trend.png"
 png(plotname, width = 2200, height = 1800, res = 300)
 # scatterplot of log(cv) as a function of log(mean expression)
-plot(X1[endog], Y1[endog],
+plot(X1, Y1,
   xlab = "log2(mean gene expression)", ylab = "log2(coefficent of variation)",
   main = "Gene dispersion as a function of their mean expression (Mean-variance trend)
      Counts are not normalized", pch = ".", cex = 2, col = "#00000055", cex.main = 1
@@ -281,10 +233,7 @@ legend("topright", legend = c("Regression line", "Poisson distribution"), lty = 
 dev.off()
 
 
-
-
-
-###   Ranking plots for cell filtering   ###
+# Ranking plots for cell filtering ####
 # Number of detected genes:
 NODG <- colSums(umi_counts > 0)
 plotname <- file_name %&% ".nodgs_cell_ranking.png"
@@ -296,7 +245,7 @@ plot(rank(-NODG), NODG, pch = 19, xlab = "Cell rank", main = "Cell ranking accor
 dev.off()
 
 
-# Fraction of MT reads per cell:
+# Fraction of MT reads per cell ####
 fractionMTreads <- colSums(umi_counts[mt, ]) / colSums(umi_counts)
 plotname <- file_name %&% ".cell_ranking_mt_reads.png"
 png(plotname, width = 2200, height = 1800, res = 300)
@@ -315,5 +264,4 @@ ggplot(data = df_fractionMTreads, aes(x = df_fractionMTreads$rank, y = df_fracti
     name = "", values = c(1, 1, 1),
     guide = guide_legend(override.aes = list(color = c("red", "orange", "blue")))
   )
-
 dev.off()
